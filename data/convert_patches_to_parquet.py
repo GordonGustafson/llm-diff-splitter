464176163f71985b1f7abfca2d346f885ce45833
Gordon Gustafson <gordon3.14@gmail.com>
2025-03-10 22:28:07 -0700

    write output.parquet to data directory

--- data/convert_patches_to_parquet.py
+++ data/convert_patches_to_parquet.py
@@ -6,6 +6,6 @@ import pathlib
 directory_of_script = pathlib.Path(__file__).parent.resolve()
 
 directory_of_tar_files = directory_of_script / "patches"
-output_parquet_filename = Path("output.parquet")
+output_parquet_filename = directory_of_script / "output.parquet"
 
 write_parquet_from_tar_directory(directory_of_tar_files, Path(output_parquet_filename))
