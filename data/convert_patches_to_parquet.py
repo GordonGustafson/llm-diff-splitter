d909c344b89914733469ef952c2899718e034854
Gordon Gustafson <gordon3.14@gmail.com>
2025-03-10 22:14:40 -0700

    don't hardcode path in convert_patches_to_parquet.py (oops)

--- data/convert_patches_to_parquet.py
+++ data/convert_patches_to_parquet.py
@@ -1,9 +1,11 @@
 from dataset import write_parquet_from_tar_directory
 
 from pathlib import Path
+import pathlib
 
+directory_of_script = pathlib.Path(__file__).parent.resolve()
 
-directory_of_tar_files = Path("/home/ggustafson/stuff/llm-diff-splitter/data/patches/")
+directory_of_tar_files = directory_of_script / "patches"
 output_parquet_filename = Path("output.parquet")
 
 write_parquet_from_tar_directory(directory_of_tar_files, Path(output_parquet_filename))
