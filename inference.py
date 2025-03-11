30a6d1b4cfa0aaaa4ed05b26a01b023bb366e51a
Gordon Gustafson <gordon3.14@gmail.com>
2025-03-10 22:53:26 -0700

    Add inference.py

---
+++ inference.py
@@ -0,0 +1,17 @@
+from transformers import pipeline, set_seed
+
+import subprocess
+import pathlib
+
+directory_of_script = pathlib.Path(__file__).parent.resolve()
+saved_model_dir = directory_of_script / "fine_tuned_gpt2"
+
+generator = pipeline('text-generation', model=str(saved_model_dir))
+set_seed(42)
+git_diff_result = subprocess.run(['git', 'diff'], stdout=subprocess.PIPE)
+git_diff_str = git_diff_result.stdout.decode('utf-8')
+print(f"git diff str: {git_diff_str}")
+result = generator(git_diff_str, num_return_sequences=1)
+print(result)
+
+
