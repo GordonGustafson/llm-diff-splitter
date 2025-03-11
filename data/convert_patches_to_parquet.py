e2aa96bc523b42dc36464949b973986699f2d2a5
Gordon Gustafson <gordon3.14@gmail.com>
2025-03-10 22:07:30 -0700

    Add convert_patches_to_parquet.py

---
+++ data/convert_patches_to_parquet.py
@@ -0,0 +1,9 @@
+from data.dataset import write_parquet_from_tar_directory
+
+from pathlib import Path
+
+
+directory_of_tar_files = Path("/home/ggustafson/stuff/llm-diff-splitter/data/patches/")
+output_parquet_filename = Path("output.parquet")
+
+write_parquet_from_tar_directory(directory_of_tar_files, Path(output_parquet_filename))
