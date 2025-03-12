e77ca12358b6fbe715b6e6c3e4a14f54531aaa4f
Gordon Gustafson <gordon3.14@gmail.com>
2025-03-12 12:49:48 -0700

    WIP on inference.py token decoding

--- inference.py
+++ inference.py
@@ -22,6 +22,7 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
 # Load the Lora model
 model = PeftModel.from_pretrained(model, str(saved_model_dir))
+model = model.cuda()
 model.eval()
 
 #####################################
@@ -32,9 +33,12 @@ git_diff_str = git_diff_result.stdout.decode('utf-8')
 input_str = f"{git_diff_str} {END_COMBINED_DIFF_MARKER} "
 input_ids = tokenizer(input_str, return_tensors="pt", truncation=True, max_length=MAX_TOKEN_LENGTH).input_ids.cuda()
 # with torch.inference_mode():
-generated_texts = model.generate(input_ids=input_ids, max_new_tokens=2048, do_sample=True, top_p=0.9)
+generated_tokens = model.generate(input_ids=input_ids, max_new_tokens=2048,  num_return_sequences=5, do_sample=True, top_p=0.9)
+generated_texts = tokenizer.batch_decode(generated_tokens.detach().cpu().numpy(), skip_special_tokens=True)
 
 print(f"input str: {input_str}")
+
+
 
 for generated_text in generated_texts:
     print(f"generated_text: {generated_text['generated_text'][len(input_str):]}")
