0dde259d260f07710d721192f366d56921c3ca9f
Gordon Gustafson <gordon3.14@gmail.com>
2025-03-13 14:31:11 -0700

    Add `time` command to some setup.sh script calls

--- setup.sh
+++ setup.sh
@@ -8,12 +8,12 @@ git clone https://github.com/GordonGustafson/llm-diff-splitter.git
 cd llm-diff-splitter
 pip install -r <(grep -v 'torch==' requirements.txt | grep -v 'torchvision==')
 
-bash data/clone_repos.sh
-bash data/save_patches.sh
-python data/convert_patches_to_parquet.py
+time bash data/clone_repos.sh
+time bash data/save_patches.sh
+time python data/convert_patches_to_parquet.py
 
 huggingface-cli login
 huggingface-cli download meta-llama/Llama-3.2-1B
 
-python train.py
+time python train.py
 
