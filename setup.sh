1a28337637fef780bf374e42057071d54f3b3b66
Gordon Gustafson <gordon3.14@gmail.com>
2025-03-14 15:42:47 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,13 +7,15 @@ nvidia-smi
 git clone https://github.com/GordonGustafson/llm-diff-splitter.git
 cd llm-diff-splitter
 pip install -r <(grep -v 'torch==' requirements.txt | grep -v 'torchvision==')
+apt-get install parallel -y
 
 time bash data/clone_repos.sh
-time bash data/save_patches.sh
+time bash data/run_save_patches_in_parallel.sh
 time python data/convert_patches_to_parquet.py
 
 huggingface-cli login
 huggingface-cli download meta-llama/Llama-3.2-1B
 
 time python train.py
+time python train_with_rl.py
 
