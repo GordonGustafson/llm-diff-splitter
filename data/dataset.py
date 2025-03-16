1e9742a5f8640e3b533741fa8e93055ab6e11ae5
Gordon Gustafson <gordon3.14@gmail.com>
2025-03-16 14:00:53 -0700

    Forgot to commit dataset.py (oops)

--- data/dataset.py
+++ data/dataset.py
@@ -61,11 +61,15 @@ def diff_pair_from_dict(dic: dict[str, str]) -> DiffPair:
 
 def get_prompt(dic: dict[str, str]) -> dict[str, str]:
     text = f"{dic['combined_diff']}{END_COMBINED_DIFF_MARKER}"
-    return {"text": text}
+    return {"prompt": text}
+
+def get_completion(dic: dict[str, str]) -> dict[str, str]:
+    text = f"{dic['first_diff']}{END_FIRST_DIFF_MARKER}{dic['second_diff']}"
+    return {"completion": text}
 
 def get_prompt_and_completion(dic: dict[str, str]) -> dict[str, str]:
     text = f"{dic['combined_diff']}{END_COMBINED_DIFF_MARKER}{dic['first_diff']}{END_FIRST_DIFF_MARKER}{dic['second_diff']}"
-    return {"text": text}
+    return {"prompt_and_completion": text}
 
 def load_huggingface_dataset(parquet_path: Path) -> datasets.DatasetDict:
     return datasets.load_dataset("parquet", data_files=str(parquet_path))
