dc9ae5fd810297c7b2f75fd30dee76db14056210
Gordon Gustafson <gordon3.14@gmail.com>
2025-03-19 15:36:53 -0700

    parse_multiple_file_diffs and parse_model_output

--- diff_analyzer.py
+++ diff_analyzer.py
@@ -1,6 +1,8 @@
 from data.dataset import END_COMBINED_DIFF_MARKER, END_FIRST_DIFF_MARKER
 from dataclasses import dataclass
 
+import itertools
+
 if END_COMBINED_DIFF_MARKER != END_FIRST_DIFF_MARKER:
     raise ValueError("END_COMBINED_DIFF_MARKER != END_FIRST_DIFF_MARKER")
 DIFF_SEPARATOR = END_COMBINED_DIFF_MARKER
@@ -27,6 +29,20 @@ class FileDiff:
     hunks: list[Hunk]
 
 
+@dataclass
+class ModelOutput:
+    first_commit_diffs: list[FileDiff]
+    second_commit_diffs: list[FileDiff]
+
+
+def get_left_file_lines(hunk: Hunk) -> str:
+    return "\n".join(line[1:] for line in hunk.lines
+                     if line.startswith((" ", "-")))
+
+def get_right_file_lines(hunk: Hunk) -> str:
+    return "\n".join(line[1:] for line in hunk.lines
+                     if line.startswith((" ", "+")))
+
 def _get_empty_hunk_from_start_line(start_line) -> Hunk:
     # Example start_line: "@@ -16,7 +16,7 @@ def blah():"
     between_at_signs = start_line.removeprefix("@@ -").split("@@")[0].strip()
@@ -38,7 +54,7 @@ def _get_empty_hunk_from_start_line(start_line) -> Hunk:
                 right_num_lines=values[3],
                 lines=[])
 
-def parse_file_diff(lines: list[str]) -> tuple[FileDiff, int]:
+def parse_file_diff_from_lines(lines: list[str]) -> tuple[FileDiff, int]:
     if not lines[0].startswith("diff "):
         raise ParseError("Missing 'diff ...' on first line")
     if not lines[1].startswith("index "):
@@ -56,9 +72,10 @@ def parse_file_diff(lines: list[str]) -> tuple[FileDiff, int]:
 
     hunks = []
     current_hunk = None
-    index_of_last_line_consumed = 0
-    for index_of_last_line_consumed, line in enumerate(lines[4:]):
+    index_of_last_line_consumed = 4
+    for index_of_last_line_consumed, line in itertools.islice(enumerate(lines), 4, None):
         if line.startswith(("diff ", "index ")):
+            index_of_last_line_consumed -= 1
             break
         if line.startswith("@@"):
             if current_hunk is not None:
@@ -72,9 +89,23 @@ def parse_file_diff(lines: list[str]) -> tuple[FileDiff, int]:
     file_diff = FileDiff(left_filename=left_filename,
                          right_filename=right_filename,
                          hunks=hunks)
-    return file_diff, 4 + index_of_last_line_consumed
+    num_lines_consumed = index_of_last_line_consumed + 1
+    return file_diff, num_lines_consumed
 
+def parse_multiple_file_diffs(text: str) -> list[FileDiff]:
+    lines = text.split("\n")
+    index_of_next_line_to_parse = 0
+    result = []
+    while index_of_next_line_to_parse < len(lines):
+        file_diff, num_lines_parsed = parse_file_diff_from_lines(lines[index_of_next_line_to_parse:])
+        index_of_next_line_to_parse += num_lines_parsed
+        result.append(file_diff)
+    return result
 
+def parse_model_output(output: str) -> ModelOutput:
+    output_split = output.split(DIFF_SEPARATOR)
+    return ModelOutput(first_commit_diffs=parse_multiple_file_diffs(output_split[0]),
+                       second_commit_diffs=parse_multiple_file_diffs(output_split[1]))
 
 
 @dataclass
--- tests/test_diff_analyzer.py
+++ tests/test_diff_analyzer.py
@@ -1,11 +1,40 @@
 import unittest
 
-# Enable importing modules located in parent directory.
 import sys
 import os
+# Enable importing modules located in parent directory.
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/..")
 
-from diff_analyzer import parse_file_diff, FileDiff, Hunk
+from diff_analyzer import parse_file_diff_from_lines, FileDiff, Hunk, parse_multiple_file_diffs, DIFF_SEPARATOR, \
+    parse_model_output, ModelOutput
+
+_TWO_FILES_DIFF_STR = """diff --git a/file1.txt b/file1.txt
+index 88de97e..7ca04a1 100644
+--- a/file1.txt
++++ b/file1.txt
+@@ -1,1 +1,1 @@
++I am file1
+diff --git a/file2.txt b/file2.txt
+index 88de97e..7ca04a1 100644
+--- a/file2.txt
++++ b/file2.txt
+@@ -1,1 +1,1 @@
++I am file2"""
+
+_TWO_FILES_DIFF_RESULT = [FileDiff(left_filename="a/file1.txt",
+                                   right_filename="b/file1.txt",
+                                   hunks=[Hunk(left_start_line_number=1,
+                                                left_num_lines=1,
+                                                right_start_line_number=1,
+                                                right_num_lines=1,
+                                                lines=["+I am file1"])]),
+                          FileDiff(left_filename="a/file2.txt",
+                                    right_filename="b/file2.txt",
+                                    hunks=[Hunk(left_start_line_number=1,
+                                                left_num_lines=1,
+                                                right_start_line_number=1,
+                                                right_num_lines=1,
+                                                lines=["+I am file2"])])]
 
 class TestDiffAnalyzer(unittest.TestCase):
     def test_parse_file_diff(self):
@@ -31,7 +60,7 @@ index 88de97e..7ca04a1 100644
  model = PeftModel.from_pretrained(model, str(saved_model_dir))
 """
         split_diff_str = diff_str.split("\n")
-        result = parse_file_diff(split_diff_str)
+        result = parse_file_diff_from_lines(split_diff_str)
         expected_result = FileDiff(left_filename="a/inference.py",
                                    right_filename="b/inference.py",
                                    hunks=[
@@ -58,8 +87,19 @@ index 88de97e..7ca04a1 100644
  # Load the Lora model
  model = PeftModel.from_pretrained(model, str(saved_model_dir))
 """.split("\n"))])
-        assert result == (expected_result, len(split_diff_str) - 1)
+        assert result == (expected_result, len(split_diff_str))
 
+    def test_parse_multiple_file_diffs(self):
+        result = parse_multiple_file_diffs(_TWO_FILES_DIFF_STR)
+        expected_result = _TWO_FILES_DIFF_RESULT
+        assert result == expected_result
+
+    def test_parse_model_output(self):
+        model_output_str = DIFF_SEPARATOR.join([_TWO_FILES_DIFF_STR, _TWO_FILES_DIFF_STR])
+        result = parse_model_output(model_output_str)
+        expected_result = ModelOutput(first_commit_diffs=_TWO_FILES_DIFF_RESULT,
+                                      second_commit_diffs=_TWO_FILES_DIFF_RESULT)
+        assert result == expected_result
 
 if __name__ == '__main__':
     unittest.main()
